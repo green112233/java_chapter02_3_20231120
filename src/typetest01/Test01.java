eb02f095fa848981f944f84422c43d0590901f54
green112233 <sunjong0930@naver.com>
2023-11-20 10:50:05 +0900

    정수타입예제 추가

---
+++ src/typetest01/Test01.java
@@ -0,0 +1,19 @@
+package typetest01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 3;
+		double result;
+		
+		result = a/b;
+		System.out.println(result);
+		
+		String str = "200";
+		int value = Integer.parseInt(str);
+		System.out.println(value);
+	}
+
+}
---
+++ src/typetest01/Test02.java
@@ -0,0 +1,22 @@
+package typetest01;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "123";
+		String str2 = "123.123";
+					
+		int s1 = Integer.parseInt(str1);
+		double d1 = Double.parseDouble(str2);
+		
+		System.out.println(s1);
+		System.out.println(d1);
+		
+		int i1 = 1234;
+		
+		String str3 = String.valueOf(i1);
+		System.out.println(str3);
+	}
+
+}
